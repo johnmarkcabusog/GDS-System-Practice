332b8222bdc00462c2b709a8eac559d5d33850ff
johnmarkcabusog <johnmarkcabusog@gmail.com>
2021-02-28 18:28:56 +0800

    Deploy GDS

---
+++ build/precache-manifest.b140d7796aa49e2ec9025d934a4a6b72.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b1bcf0aa1f4de6df43e7973acf13f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "039aa110eccbf46dc32f",
+    "url": "/static/css/main.d24b35bf.chunk.css"
+  },
+  {
+    "revision": "e48160e6c0c284df7e88",
+    "url": "/static/js/2.d4b2cfe2.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.d4b2cfe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039aa110eccbf46dc32f",
+    "url": "/static/js/main.ec0f3050.chunk.js"
+  },
+  {
+    "revision": "fb5444777a9652c8c1f7",
+    "url": "/static/js/runtime-main.6585e54a.js"
+  },
+  {
+    "revision": "fd0c41b2a3d127d54284aa93fa708593",
+    "url": "/static/media/aerial-background.fd0c41b2.jpg"
+  },
+  {
+    "revision": "ab8de6d1e2c72beea4b41717fd47120c",
+    "url": "/static/media/hotel.ab8de6d1.jpg"
+  },
+  {
+    "revision": "796850df25c9acccf106760037ceba9c",
+    "url": "/static/media/plane.796850df.png"
+  },
+  {
+    "revision": "d46d364036055378da9370b26b45ae25",
+    "url": "/static/media/traveller.d46d3640.jpg"
+  }
+]);
